830222bf4142c76c6c398a16d941235177c1feb4
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-07-07 16:18:08 -0300

    feat: pegar o produto especifico pelo id

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/ProductsController.js";
+import { getProducts, getProduct } from "../controllers/ProductsController.js";
 
 const router = Router();
 
 router.get('/products', getProducts)
+router.get('/products/:id', getProduct)
 
 export default router;
