dcfa4bcd570b9bcc169a5a0cc8395991e22b8071
Daniel <danielnatsufire@gmail.com>
2022-07-06 23:05:15 -0300

    fix: permitindo pegar os produtos para usuário não logado, removendo validate token do router

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { getProducts } from "../controllers/ProductsController.js";
-import { validateToken } from "../middlewares/validateToken.js";
 
 const router = Router();
 
-router.get('/products', validateToken, getProducts)
+router.get('/products', getProducts)
 
 export default router;
